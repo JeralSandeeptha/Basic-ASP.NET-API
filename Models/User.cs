9be397dbc6162ff8d7abd08a6ac75db3a31bd118
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2023-11-12 15:44:27 +0530

    Create user model

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace SuperHeroAPI.Models
+{
+    public class User
+    {
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private string Email { get; set; }
+        private int Age { get; set; }
+
+    }
+}
